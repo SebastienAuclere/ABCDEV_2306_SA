3e5d00a6173c74c1d40c19959ecbaa24936e15fa
balistik23 <137295360+balistik23@users.noreply.github.com>
2023-06-29 15:44:06 +0200

    evaluation

---
+++ Algo-Java/Jalon_algo_niveau1_/src/abcdev_jalon/App.java
@@ -0,0 +1,14 @@
+package abcdev_jalon;
+
+public class App {
+
+	public static void main(String[] args) {
+		 
+		 //variables
+		 prenom = chaine de caracteres1
+		 nom = chaine de caractères2
+		 departement = nombre1 est un entier
+
+	}
+
+}

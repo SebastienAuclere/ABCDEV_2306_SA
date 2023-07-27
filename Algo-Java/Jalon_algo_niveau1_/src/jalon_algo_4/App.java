3765a7e4cdcc70acc0bea415df9f25acdf804836
balistik23 <137295360+balistik23@users.noreply.github.com>
2023-07-27 12:54:04 +0200

    jalon

---
+++ Algo-Java/Jalon_algo_niveau1_/src/jalon_algo_4/App.java
@@ -0,0 +1,46 @@
+package jalon_algo_4;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		double position;
+		double nb;
+		String resultat;
+		double nbPrecedent;
+		double nbCourant;
+		double nbSuivant;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("combien voulez vous afficher de nombres: ");
+		nb = sc.nextDouble();
+		position = 2;
+		resultat = "\t0\n\t1";
+		nbPrecedent = 0;
+		nbCourant = 1;
+		
+		if (nb>2); 
+		{
+			  while  ( position < nb );
+			    {			 
+			 
+				  nbSuivant = nbPrecedent + nbCourant;
+				  resultat = resultat + "\n\t" + nbSuivant;
+				  nbPrecedent = nbCourant;
+				  nbCourant = nbSuivant;
+				  position = position + 1;
+				}
+		   
+			  do 
+			   {
+				  nb = 2;		
+				  
+			   }
+	  else if
+			  System.out.println("les " + nb + "premiers nombres de la suite fibonacci sont \n " + resultat );
+		}
+		sc.close();
+	}
+}
---
+++ Algo-Java/projet_exercice/src/tableau_3_3/App.java
@@ -0,0 +1,10 @@
+package tableau_3_3;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
